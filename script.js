c3b20c3f03d5120941e561074d64f061660bbc3b
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 11:21:02 -0800

    Add correct number of boxes on each side
    The create grid function will now make each side have size number of
    boxes. Also fixed an issue where clicking cancel in the prompt would
    delete the grid.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let currentSize = 16;
+
 function createGrid(size) {
     const container = document.querySelector(".container");
     removeGrid(container);
-    const sideSize = 960 / size;
+    // Since I have borders on the boxes I am removing about that amount of 
+    // pixels from the width of the container, so we will get {size} number of 
+    // boxes on each side
+    const sideSize = (960 - 2 * size) / size;
     for (let i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
             const div = document.createElement("div");
@@ -11,6 +16,15 @@ function createGrid(size) {
             container.append(div);
         }
     }
+    const boxList = document.querySelectorAll(".box");
+    boxList.forEach((box) => {
+        box.addEventListener("mouseenter", (e) => {
+            e.target.classList.toggle("hover");
+        });
+        box.addEventListener("mouseleave", (e) => {
+            e.target.classList.toggle("hover");
+        });
+    });
 }
 
 function removeGrid(container) {
@@ -21,18 +35,9 @@ function removeGrid(container) {
 
 createGrid(16);
 
-const boxList = document.querySelectorAll(".box");
-boxList.forEach((box) => {
-    box.addEventListener("mouseenter", (e) => {
-        e.target.classList.toggle("hover");
-    });
-    box.addEventListener("mouseleave", (e) => {
-        e.target.classList.toggle("hover");
-    });
-});
-
 function setGridSize() {
-    const size = prompt("How many boxes per side?", 16);
+    const size = prompt("How many boxes per side?", 16) || currentSize;
+    currentSize = size;
     createGrid(size);
 }
 
