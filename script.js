0fca8aef5b6d5465236a127bc27c70aa067f4e68
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 12:03:21 -0800

    Add size constraints, colors and darken on hover
    The prompt will now check for the number to be between 1-100, if they
    enter anything that is not one of those numbers the prompt asks again.
    Now on hover of a box, a random color is generated and the opacity
    darkens by 0.1.

--- script.js
+++ script.js
@@ -19,13 +19,17 @@ function createGrid(size) {
     const boxList = document.querySelectorAll(".box");
     boxList.forEach((box) => {
         box.addEventListener("mouseenter", (e) => {
-            e.target.classList.toggle("hover");
-        });
-        box.addEventListener("mouseleave", (e) => {
-            e.target.classList.toggle("hover");
+            e.target.style.backgroundColor = `rgb(${getRandomRGBNumber()}, ${getRandomRGBNumber()}, ${getRandomRGBNumber()})`;
+            if (Number(e.target.style.opacity) < 1) {
+                e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
+            }
         });
     });
 }
+
+function getRandomRGBNumber() {
+    return Math.floor(Math.random() * 256);
+  }
 
 function removeGrid(container) {
     while(container.firstChild) {
@@ -36,9 +40,14 @@ function removeGrid(container) {
 createGrid(16);
 
 function setGridSize() {
-    const size = prompt("How many boxes per side?", 16) || currentSize;
-    currentSize = size;
-    createGrid(size);
+    const size = Number(prompt("How many boxes per side? 100 is the max, 1 is the min.", 16) || currentSize);
+    if (Number.isInteger(size) && size <= 100 && size > 0) {
+        currentSize = size;
+        createGrid(size);
+    } else {
+        setGridSize();
+    }
+    
 }
 
 document.querySelector(".grid-button").addEventListener("click", setGridSize);
